866d68e53c9686a9e1cc8f74e2551d408f6df5b2
ayush <ayush.tiwari@knoldus.in>
2017-12-12 00:13:23 +0530

    refactoring

--- src/main/scala/com/tiwari/scala/storm/RandomSentenceSpout.scala
+++ src/main/scala/com/tiwari/scala/storm/RandomSentenceSpout.scala
@@ -16,10 +16,10 @@ class RandomSentenceSpout extends BaseRichSpout {
 
     Thread.sleep(100)
 
-    val sentences = Array("Knol means a unit of knowledge and Dus comes from Druksh which in sanskrit means tree","Hence, Knoldus stands for tree of knowledge",
-      "It is an inspiration that we live with when we build products which deliver high quality business value and share our knowledge extensively via our blogs, conferences, meetups, books and code",
-      "Knol means a unit of knowledge and Dus comes from Druksh which in sanskrit means tree","Hence, Knoldus stands for tree of knowledge",
-      "It is an inspiration that we live with when we build products which deliver high quality business value and share our knowledge extensively via our blogs, conferences, meetups, books and code")
+    val sentences = Array("Knol means a unit of knowledge and Dus comes from Druksh which in sanskrit means tree", "Hence, Knoldus stands for tree of knowledge",
+       "It is an inspiration that we live with when we build products which deliver high quality business value and share our knowledge extensively via our blogs, conferences, meetups, books and code",
+      "Knol means a unit of knowledge and Dus comes from Druksh which in sanskrit means tree", "Hence, Knoldus stands for tree of knowledge",
+       "It is an inspiration that we live with when we build products which deliver high quality business value and share our knowledge extensively via our blogs, conferences, meetups, books and code")
 
     val sentence = sentences(random.nextInt(sentences.length))
     spoutOutputCollector.emit(new Values(sentence))
@@ -30,8 +30,13 @@ class RandomSentenceSpout extends BaseRichSpout {
     random = Random
   }
 
+  override def ack(id: Any) {
+  }
+
+  override def fail(id: Any){
+  }
+
   override def declareOutputFields(declarer: OutputFieldsDeclarer): Unit = {
     declarer.declare(new Fields("word"))
   }
-
 }
